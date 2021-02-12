a1f195e29f6a743f24a6e0d1360ebf0d16805e9c
gHenriquez99 <henriquez.gus99@gmail.com>
2021-02-12 13:08:46 -0500

    managed use to .map, not sure if it is working but it's there!

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -5,12 +5,30 @@
 // document.querySelector('#div1').onclick = doTheThing;
 
 // .foreach and .map are the array methods we should be using
+const i = 1;
+// let labeled = carousel.querySelectorAll("li").array.map(element => {
+//     element.style.position = "relative";
+// });
+
+const images = carousel.querySelectorAll('li');
+const imageArray = Array.from(images);
+
+const newArray = imageArray.map((element) => {
+  const listItem = document.createElement('li');
+  listItem.innerText = element;
+  listItem.style.position = 'relative';
+  listItem.insertAdjacentHTML('beforeend', '<span style="position:absolute;left:0;top:0">1</span>');
+  return listItem;
+});
+
+console.log(newArray);
 
 const width = 130;
 const count = 3;
 
 const list = carousel.querySelector('ul');
-const listElems = carousel.querySelectorAll('li');
+// const listElems = carousel.querySelectorAll('li');
+const listElems = newArray;
 
 let position = 0;
 
